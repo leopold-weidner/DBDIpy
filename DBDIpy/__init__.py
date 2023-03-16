a860d438971a374d222dd21e7315cfa715bad439
Leopold Weidner <81673643+leopold-weidner@users.noreply.github.com>
2023-03-16 15:07:20 +0100

    new fun

--- DBDIpy/__init__.py
+++ DBDIpy/__init__.py
@@ -3,3 +3,4 @@ from DBDIpy.export_to_spectra import export_to_spectra
 from DBDIpy.identify_adducts import identify_adducts
 from DBDIpy.impute_intensities import impute_intensities
 from DBDIpy.plot_adducts import plot_adducts 
+from DBDIpy.propose_adducts import propose_adducts
